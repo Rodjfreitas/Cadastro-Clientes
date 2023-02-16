ba4126a6ff132e128900bb4f39d0d244578a70d1
Rodjfreitas <rodrigofreitas2011@live.com>
2023-02-16 14:18:05 -0300

    feat & style: adicionando campos de e-mail e cpf

--- js/ambiente.js
+++ js/ambiente.js
@@ -4,11 +4,13 @@ let cidade = document.querySelector('.city')
 let estado = document.querySelector('.state')
 let nascimento = document.querySelector('.birth')
 let telefone = document.querySelector('.phone')
+let email = document.querySelector('.mail')
+let cpf = document.querySelector('.cpf')
 let tabela = document.querySelector('table')
 let valores = []
 
-function verifield(n,a,c,s,b,p){
-  if(n == 0 || a == 0 || c == 0 || s == 0 || b ==0 || p == 0){
+function verifield(n,a,c,s,b,p,e,f){
+  if(n == 0 || a == 0 || c == 0 || s == 0 || b ==0 || p == 0 || e ==0 || f == 0){
     return true
   }else{
     return false
@@ -17,20 +19,23 @@ function verifield(n,a,c,s,b,p){
 }
 
 function cadastro(){
-  if(verifield(nome.value.length, endereco.value.length, cidade.value.length, estado.value.length, nascimento.value.length, telefone.value.length)){
+  if(verifield(nome.value.length, endereco.value.length, cidade.value.length, estado.value.length, nascimento.value.length, telefone.value.length, email.value.length, cpf.value.length)){
     alert('Existem campos que não foram preenchidos!')
   }else{
-    valores.push(nome.value, endereco.value, cidade.value, estado.value, nascimento.value, telefone.value)    
+    valores.push(nome.value, endereco.value, cidade.value, estado.value, nascimento.value, telefone.value, email.value, cpf.value )    
     
     let linha = document.createElement('tr')
 
     for(pos in valores){
       let item = document.createElement('td')
+      item.style.fontSize = "13px"
       item.setAttribute('class','listas')
       item.append(valores[pos]) 
       linha.append(item) 
       tabela.append(linha)     
     }
+
+
     valores = []
     nome.value = ""
     endereco.value = ""
@@ -38,6 +43,8 @@ function cadastro(){
     estado.value = ""
     nascimento.value = ""
     telefone.value = ""
+    email.value = ""
+    cpf.value = ""
     nome.focus()
     
     
